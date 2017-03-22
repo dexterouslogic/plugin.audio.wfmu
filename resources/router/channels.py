a26e2a9b69cdbcdd3d094223c44145b0f54a77f5
sos <technicalflaw@users.noreply.github.com>
2017-03-22 13:37:03 +0000

    Allow access to all stream bitrates. Bump version

--- resources/router/channels.py
+++ resources/router/channels.py
@@ -6,29 +6,32 @@ import requests
 from resources.constants import Const
 # from resources.lib import requests
 
+
 class Router:
     def __init__(self, addon):
         self.addon_id = addon.getAddonInfo('id')
-    
+
     def _get_list_items(self, channels, bitrate=None, default_channel_id=None):
         """Get iterator for live streams (channels)"""
-        def _get_streams(channel, format):
-            """Get a stream from a channel element. Default to highest bitrate"""
-            # attribute expression not supported in python 2.6, so this won't work
+        def _get_streams(channel, fmt='mp3'):
+            """Get a stream from a channel element"""
+            # attribute expression not supported in python 2.6,
+            # so this won't work
             # xpath = "streams/stream[@format='{0}']".format(format)
             # if bitrate:
             #     xpath += "[bitrate='{:d}']".format(bitrate)
             for stream in channel.findall("streams/stream"):
-                if not format or stream.attrib.get('format') != format:
+                if fmt and stream.attrib.get('format') != fmt:
                     continue
-                if bitrate and int(stream.attrib.get('bitrate')) != bitrate:
+
+                if bitrate and int(stream.findtext("bitrate")) != bitrate:
                     continue
-        
+
                 yield stream
-    
+
         # special://home/addons/.../resources/
         thumbnails_base_url = Const.RESOURCES_URL_FORMAT.format(self.addon_id) + "media/thumbnails/"
-    
+
         for c in channels:
             # any context will do
             title = c.find("titles/title").text
@@ -37,46 +40,68 @@ class Router:
             # find the stream with the default bitrate
             # streams = findall_streams(channel, bitrate=bitrate)
             # if not streams:
-            streams = _get_streams(c, format='mp3')
+            streams = _get_streams(c)
 
-            # choose highest rate stream by default
-            try:
-                stream = max(streams, key=lambda stream: int(stream.findtext('bitrate')))
-            except ValueError:
-                # requires >= 1 streams
-                continue
-            else:
+            # yield all streams (not just highest rate)
+            for stream in streams:
                 # thumbnails are stored in resources/media/thumbnails/$ID.png
                 path = stream.findtext('directUrl')
                 thumbnail = "{0}{1:d}.png".format(thumbnails_base_url, id)
+                br = int(stream.findtext("bitrate"))
 
                 yield {
-                    'label': title,
+                    'label': "{0} [{1}Kb/s]".format(title, br),
                     'path': path,
                     'thumbnail': thumbnail,
-                    # 'selected': id == default_channel_id,
                     'is_playable': True,
-                    #'icon': stream.findtext('iconUrl'),
-                    # 'info': {
-                    #     'count': int(stream.findtext('bitrate'))
-                    # },
                     'stream_info': {
                         'codec': stream.attrib.get('format')
                     }
-                }    
-            
+                    # 'selected': id == default_channel_id,
+                    # 'icon': stream.findtext('iconUrl'),
+                    # 'info': {
+                    #     'count': int(stream.findtext('bitrate'))
+                    # },
+                }
+
+            # choose highest rate stream by default
+            # try:
+            #     stream = max(streams, key=lambda s: int(s.findtext('bitrate')))
+            # except ValueError:
+            #     # requires >= 1 streams
+            #     continue
+            # else:
+            #     # thumbnails are stored in resources/media/thumbnails/$ID.png
+            #     path = stream.findtext('directUrl')
+            #     thumbnail = "{0}{1:d}.png".format(thumbnails_base_url, id)
+
+            #     yield {
+            #         'label': title,
+            #         'path': path,
+            #         'thumbnail': thumbnail,
+            #         # 'selected': id == default_channel_id,
+            #         'is_playable': True,
+            #         #'icon': stream.findtext('iconUrl'),
+            #         # 'info': {
+            #         #     'count': int(stream.findtext('bitrate'))
+            #         # },
+            #         'stream_info': {
+            #             'codec': stream.attrib.get('format')
+            #         }
+            #     }
+
     def route(self):
         """Get a iter of items representing each channel's live stream
         see: http://wfmu.org/channels_feed.php?xml=1
         """
         url = urlparse.urljoin(Const.BASE_URL, "channels_feed.php")
-        payload = { 
-            'xml': 1 
+        payload = {
+            'xml': 1
         }
 
-        r = requests.get(url, params=payload, stream=False) 
+        r = requests.get(url, params=payload, stream=False)
         r.raise_for_status()
-        
+
         root = ET.fromstring(r.content)
 
         # every channel contains multiple streams
